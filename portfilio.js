25f7de10dfc1ed8d7bf8bb6f7d81b5d328484840
anna61e2 <anna61e2@stud.kea.dk>
2023-01-11 01:54:43 +0100

    first commit

---
+++ portfilio.js
@@ -0,0 +1,28 @@
+/*
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = docment.querySelectorAll(".carousel-slide img");
+
+//Buttons//
+
+const prevBtn = docment.querySelector("#prevBtn");
+const nextBtn = docment.querySelector("#nextBtn");
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+//Button listner
+
+nextBtn.addEventListener("click", () => {
+  carouselSlide.style.transition = "transform 0.4s ease-in.out";
+  counter++;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+prevBtn.addEventListener("click", () => {
+  carouselSlide.style.transition = "transform 0.4s ease-in.out";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+}); */
